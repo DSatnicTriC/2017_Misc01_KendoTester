a54074d028a43694ee5d5d919458b026314ff9b7
achoo54321 <actionnotmotion@gmail.com>
2017-02-13 20:49:49 -0500

    Added Microsoft System.Web.Optimization for bundling scripts.  Created a debug and release bundle that loads the full or min version of the script depending on the type of build

--- DanielWebApp02/Global.asax.cs
+++ DanielWebApp02/Global.asax.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace DanielWebApp02
 {
@@ -13,6 +10,24 @@ namespace DanielWebApp02
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterBundles(BundleTable.Bundles);
+        }
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            ScriptBundle debugBundle = new ScriptBundle("~/bundles/debugBundle");
+            debugBundle.Include(
+                "~/Scripts/jquery-1.10.2.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/kendo.ui.core.js");
+            bundles.Add(debugBundle);
+
+            ScriptBundle releaseBundle = new ScriptBundle("~/bundles/releaseBundle");
+            releaseBundle.Include(
+                "~/Scripts/jquery-1.10.2.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/kendo.ui.core.min.js");
+            bundles.Add(releaseBundle);
         }
     }
 }
---
+++ DanielWebApp02/StaticHelpers/HtmlHelpers.cs
@@ -0,0 +1,14 @@
+﻿namespace DanielWebApp02.StaticHelpers
+{
+    public static class HtmlHelpers
+    {
+        public static bool UsingDebugConfiguration()
+        {
+            #if DEBUG
+                return true;
+            #else
+                return false;
+            #endif
+        }
+    }
+}
