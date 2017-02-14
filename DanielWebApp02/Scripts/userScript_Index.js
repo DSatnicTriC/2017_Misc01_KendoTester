cc648976817d6d7abce5d65bfc21647f3be1fbf6
achoo54321 <actionnotmotion@gmail.com>
2017-02-13 22:39:28 -0500

    Successfully initialized a Kendo Date Picker and Button and figured out which .js and .css I need

---
+++ DanielWebApp02/Scripts/userScript_Index.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    function submitClick(e) {
+        alert("I was clicked");
+    }
+
+    $("#myDayPicker").kendoDatePicker();
+    $("#submitButton").kendoButton({
+        click: submitClick
+    });
+});
